615f259ef117fe07e3ac30003f85ebe08811da66
frmnjn <firmanbudis@gmail.com>
2020-03-29 22:30:11 +0700

    update

--- index.js
+++ index.js
@@ -14,51 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 let data = {
-  // id: "asd",
-  fileId: "asd",
-  fileIdName: "asd",
-  fileTypeId: "asd",
-  fileFormatId: "asd",
-  regional: "asd",
-  protocolType: 3,
-  ip: "asd",
-  username: "asd",
-  password: "asd",
-  port: 21,
-  remotePath: ["asd"],
-  localPath: "asd",
-  processType: "asd",
-  processPath: "asd",
-  pattern: "asd",
-  cron: "asd",
-  createdBy: "asd",
-  createdDate: "2019-01-01T15:21:13.310+0000",
-  active: true,
-  collecting: false,
-  writeToFile: true,
-  defaultFieldValue: '{ "defaultAreaCode" : "021" }',
-  fileTypeTransfer: 2,
-  logToDB: false,
-  logToFile: true,
-  isAgregate: false,
-  patternForOutput: "",
-  specialCondition: "",
-  forceRegexInput: "",
-  forceRegexTake: "",
-  isBypass: "0",
-  fileDispatchId: "",
-  delimiter: "",
-  cacValidation: "",
-  cekAnotherSource: "0",
-  isRenamed: "0",
-  isArchived: "0",
-  renameTo: "",
-  hdfsPath: "/data/input/hjtn2/",
-  lookProcess: "2",
-  saveProcess: "2",
-  compressInput: "0",
-  worker: "211,222,244,311,322,333,344,411,422,433,444",
-  _class: "com.os.neta.entities.config.model.FILE_DETAIL_INPUT"
+  data: "TEST_DATA"
 };
 
 app.get("/api", (req, res) => {
@@ -67,7 +23,7 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/api_ribet", (req, res) => {
-  console.log("api_ribet called");
+  console.log("GET api_ribet called");
   // cek dia ngasih header auth sama username ngga
   if (req.headers.authorization && req.query.username) {
     const authHeader = req.headers.authorization;
@@ -92,15 +48,45 @@ app.get("/api_ribet", (req, res) => {
   }
 });
 
-app.get("/get-token/:username", (req, res) => {
-  const username = req.params.username;
+app.post("/api_ribet", (req, res) => {
+  console.log("POST api_ribet called");
+  // cek dia ngasih header auth, body username sama password ngga
+  if (req.headers.authorization && req.body.username && req.body.password) {
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(" ")[1];
+    const username = req.body.username;
+    const password = req.body.password;
+
+    jwt.verify(token, process.env.TOKEN_SECRET, function(err, decoded) {
+      if (!err) {
+        // cek auth bener apa salah
+        if (decoded.username == username && decoded.password == password) {
+          // cek username password nya bener ngga
+          res.status(200).json(data);
+        } else {
+          res.status(401).send("Authorization failed!");
+        }
+      } else {
+        res.status(401).send(err);
+      }
+    });
+  } else {
+    res.status(403).send("You are not authorized!");
+  }
+});
+
+app.post("/get-token", (req, res) => {
+  console.log(req.body);
+  const username = req.body.username;
+  const password = req.body.password;
   console.log("created token for", username);
   const token = jwt.sign(
     {
-      username: username
+      username: username,
+      password: password
     },
     process.env.TOKEN_SECRET,
-    { expiresIn: "1h" }
+    { expiresIn: "12h" }
   );
 
   res.status(200).send(token);
