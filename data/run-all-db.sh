c3c9be42347f3f51d157c5197049ba1016be4dd1
frmnjn <firmanbudis@gmail.com>
2020-03-31 09:45:45 +0700

    add data & script

---
+++ data/run-all-db.sh
@@ -0,0 +1,3 @@
+sudo docker run --name postgres -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
+sudo docker run --name mongo -p 27017:27017 -d mongo
+sudo docker run --name mysql -e MYSQL_ROOT_PASSWORD=secr3t123 -e MYSQL_DATABASE=testdb -e MYSQL_USER=admin -e MYSQL_PASSWORD=admin -p 3306:3306 -d mysql --default-authentication-plugin=mysql_native_password
