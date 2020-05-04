b7f401bfd3cb451572199b145ee103a389f9b393
Firman Budi Safrizal <firmanbudis@gmail.com>
2020-05-04 10:39:07 +0700

    tambah sql buat oracle

--- data/create table actor.sql
+++ data/create table actor.sql
@@ -1,6 +1,10 @@
-CREATE TABLE actor (
+CREATE TABLE TESTDB.actor (
 	actor_id int,
 	first_name varchar(50),
 	last_name varchar(50),
 	last_update timestamp
 );
+
+--buat oracle
+--ALTER USER TESTDB QUOTA 100M ON SYSTEM;
+--GRANT UNLIMITED TABLESPACE TO TESTDB;
