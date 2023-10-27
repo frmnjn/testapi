5d0cb78855481dc42f022b7785c8720a12d578dd
Firman Budi Safrizal <firmanbudis@gmail.com>
2023-10-27 10:04:03 +0700

    add setup oracle user & db in docker-run-all-db.sh

--- script/docker-run-all-db.sh
+++ script/docker-run-all-db.sh
@@ -5,3 +5,31 @@ sudo docker run --name mysql -e MYSQL_ROOT_PASSWORD=secr3t123 -e MYSQL_DATABASE=
 sudo docker run --name mssql -e ACCEPT_EULA=Y -e SA_PASSWORD=Hesemeleh_123 -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
 #dafault -- database=xe, username=system, password=oracle
 sudo docker run --name oracledb -p 1521:1521 -d oracleinanutshell/oracle-xe-11g
+
+# Setup Oracle DB
+#select * from dba_tables;
+#SELECT table_name FROM all_tables WHERE owner ='FRMNJN';
+
+#CREATE USER frmnjn IDENTIFIED BY password;
+#GRANT CREATE TABLE TO frmnjn;
+#grant sysdba to frmnjn;
+#GRANT CONNECT TO frmnjn;
+#GRANT CREATE SESSION TO frmnjn;
+#GRANT UNLIMITED TABLESPACE TO frmnjn;
+
+#GRANT create session TO frmnjn;
+#GRANT create table TO frmnjn;
+#GRANT create view TO frmnjn;
+#GRANT create any trigger TO frmnjn;
+#GRANT create any procedure TO frmnjn;
+#GRANT create sequence TO frmnjn;
+#GRANT create synonym TO frmnjn;
+#GRANT CONNECT, RESOURCE, DBA TO frmnjn;
+
+#SELECT * FROM dba_users
+#WHERE username = 'FRMNJN';
+
+#CREATE SCHEMA AUTHORIZATION FRMNJN;
+#CREATE TABLE TEST_SCHEMA.TEST_TABLE (
+#    ID VARCHAR2(40) NOT NULL
+#    )
